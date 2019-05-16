f4423bc2f7eac51d341f889f26ae0fe6ff24cbe9
mlvzk <mlvzk@protonmail.com>
2019-05-16 10:25:24 +0200

    rename shadowed variable 'append'

--- cmd/tee/main.go
+++ cmd/tee/main.go
@@ -28,13 +28,13 @@ func main() {
 
 	command, _ := parser.Parse(os.Args)
 
-	append := command.Booleans["append"]
+	shouldAppend := command.Booleans["append"]
 
 	var err error
 	files := make([]io.Writer, len(command.Positionals)+1)
 	files[len(command.Positionals)] = os.Stdout
 	for i, arg := range command.Positionals {
-		if append {
+		if shouldAppend {
 			if files[i], err = os.OpenFile(arg, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600); err != nil {
 				log.Fatalln(errors.Wrapf(err, "failed to open file '%s'", arg))
 			}
